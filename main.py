5705d59a2311b9774761cfef099b29280cc40636
Samuel Johnson <samueljohnson01997@gmail.com>
2025-01-27 10:31:30 +0530

    feat: range of periods for momentum

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from NiftyDataLoader import NiftyDataLoader
 from data_processor import DataProcessor
 from strategy.momentum_strategy import MomentumStrategy
+import pandas as pd
 
 def main():
     loader = NiftyDataLoader(csv_path="data/ind_nifty500list.csv")
-    strategy = MomentumStrategy(period = 100)
-    processor = DataProcessor(loader, strategy)
-
     clean_data_path = "data/filtered_nifty500.csv"
     # The below line is commented as the clean data for nifty500list.csv is created. If need a new one, uncomment and get the data
     # loader.create_clean_data(clean_data_path)
 
-    top_symbols = processor.processTop(clean_data_path)
-    print("Top Symbols")
-    print(top_symbols)
+    df = loader.read_clean_data(filepath=clean_data_path)
+    periods = [100, 75, 50, 25]
+    top_symbols_dict = {}
 
-    bottom_symbols = processor.processBottom(clean_data_path)
-    print("Bottom Symbols")
-    print(bottom_symbols)
+    for period in periods:
+        strategy = MomentumStrategy(period=period)
+        top_symbols = strategy.screen_top(df)
+        top_symbols_dict[period] = top_symbols
 
+        print(f"\nTop Symbols for period {period}\n")
+        print(top_symbols)
+
+    # Convert the dictionary to a DataFrame with symbols and percentage values
+    top_symbols_df = pd.concat(top_symbols_dict, axis=1)
+    print("\nTop Symbols DataFrame\n")
+    print(top_symbols_df)
 
 if __name__ == "__main__":
     main()
